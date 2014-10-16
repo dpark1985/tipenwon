02fc1d8795a14c92910b8a53c6d7ede595bd44c0
Daniel Park <daniel@inswave.com>
2014-10-17 02:13:58 +0900

    ...,...

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
+/* Connect mysql */
+var client = mysql.createConnection({
+	user: 'root',
+	password: 'dp980605',
+	database: 'tipenwon'
+});
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
-  //console.log("testing");
+  
+});
+
+router.post('/', function(req, res){
+	var data = req.body;
+	
+	client.query('SELECT id, pw FROM memberships WHERE (id=data.id) AND (pw=data.pw)', function(error, result, fields){
+		if(error){
+			socket.emit('message", 'Please Check again');
+		}else{
+			
+		}
 });
 
 module.exports = router;
