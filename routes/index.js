07e55e78802980803cca27f494766fc28c0ed57e
Daniel Park <daniel@inswave.com>
2014-10-17 12:27:05 +0900

    123

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 
+var errMsg = '';
+
 /* Connect mysql */
 var client = mysql.createConnection({
 	user: 'root',
@@ -11,19 +13,35 @@ var client = mysql.createConnection({
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { msg: errMsg });
   
 });
 
+/* POST home page. */
 router.post('/', function(req, res){
 	var data = req.body;
 	
+	var id = data.id;
+	var pw = data.pw;
+	
+	if(id === '' || pw === ''){
+		errMsg = "아이디와 비밀번호를 다시 확인해주세요.";
+		res.redirect('/');
+	}
+	
 	client.query('SELECT id, pw FROM memberships WHERE (id=data.id) AND (pw=data.pw)', function(error, result, fields){
 		if(error){
-			socket.emit('message", 'Please Check again');
+			socket.emit('message', 'Please Check again');
 		}else{
-			
+			res.redirect('/clickinout');
 		}
+	});					
 });
+
+/* GET home page. */
+router.get('/clickinout', function(req, res){
+	res.render('clickinout');
+});
+
 
 module.exports = router;
